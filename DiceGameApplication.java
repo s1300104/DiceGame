30b18a0f8ae53f22e998f27f3a2ca0d2b0513484
FUKAYA_Ryota <s1300104@u-aizu.ac.jp>
2023-06-10 00:28:53 +0900

    Greetings added

--- DiceGameApplication.java
+++ DiceGameApplication.java
@@ -1,11 +1,16 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class DiceGameApplication {
 
     public DiceGameApplication() {
         Random r = new Random();
+        Scanner sc = new Scanner(System.in);
         int total = 0;
         int tmp;
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
         System.out.println("Rolling dice...");
         for (int i = 1; i <= 2; i++) {
             tmp = r.nextInt(6) + 1;
@@ -14,7 +19,7 @@ class DiceGameApplication {
         }
         System.out.printf("Total value: %d\n", total);
     }
-
+    
     public static void main(String[] args) {
         new DiceGameApplication();
     }
